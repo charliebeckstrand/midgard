34c84a66451bdbc7d12bc5a66e0359e2a0dda239
Charlie Beckstrand <charlie.beckstrand@icloud.com>
2026-04-09 08:14:17 -0600

    Added overflow-x to horizontal timeline

--- packages/ui/src/recipes/katachi/timeline.ts
+++ packages/ui/src/recipes/katachi/timeline.ts
@@ -4,7 +4,7 @@ export const timeline = {
 	base: 'list-none p-0 m-0',
 	orientation: {
 		vertical: 'flex flex-col',
-		horizontal: 'flex flex-row',
+		horizontal: 'flex flex-row overflow-x-auto',
 	},
 	variant: {
 		solid: '',
